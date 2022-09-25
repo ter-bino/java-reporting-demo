cddd6d93a0e46d39f8a75a639761d5d136f963c9
teral! <alter.orbino@orangeandbronze.com>
2022-09-25 21:59:54 +0800

    Added demo b - variables

---
+++ src/b/variables/Main.java
@@ -0,0 +1,31 @@
+package b.variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Store values to variables
+		//	TODO 01.a: Declare a variable
+		
+		// 	TODO 01.b: Initialize a variable
+		
+		//	TODO 01.c: Output the value of the variable
+		
+		
+		//=====================================================================//
+		
+		// TODO 02: Initialization on decleration
+		//	TODO 02.a: Declare a variable and initialize a value
+		
+		//	TODO 02.b: Output the value of the variable
+		
+		
+		//=====================================================================//
+		
+		// TODO 03: 'var' variables + JAVA 10+
+		//	TODO 03.a: Store any data to a 'var'
+		
+		//	TODO 03.b: Output the value held by 'var'
+		
+	}
+
+}
