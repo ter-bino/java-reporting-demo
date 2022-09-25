60256a7137a5eb077ea10dab726b031b98f2e644
teral! <alter.orbino@orangeandbronze.com>
2022-09-25 22:54:41 +0800

    Added demo c - data types

---
+++ src/c/data_types/Main.java
@@ -0,0 +1,16 @@
+package c.data_types;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Primitive data types
+
+		
+		// TODO 02: Non-primitive data types
+
+		
+		// TODO 03: How reference variables work
+
+	}
+
+}
