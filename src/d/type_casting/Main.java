c94fe175ae4c3f67343dba0d7df2f70c4f220df9
teral! <alter.orbino@orangeandbronze.com>
2022-09-25 23:13:27 +0800

    Added demo d - type casting

---
+++ src/d/type_casting/Main.java
@@ -0,0 +1,26 @@
+package d.type_casting;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Wide casting (implicit)
+		//	TODO 01.a: Create a byte variable
+		
+		//	TODO 01.b: Type cast to bigger data types
+		
+		
+		//	TODO 01.c: Invalid type cast
+		
+		
+		// TODO 02: Narrow casting (explicit)
+		//	TODO 02.a: Create a float variable
+		
+		//	TODO 02.b: Type cast to smaller data types
+		
+		//	TODO 03.c: Invalid narrow casting
+		
+		
+		//System.out.println();
+	}
+
+}
