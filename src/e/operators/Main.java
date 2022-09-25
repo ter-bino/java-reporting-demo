e4eed9c54228e7b48387b5898c3e9cfa44466d24
teral! <alter.orbino@orangeandbronze.com>
2022-09-25 23:57:48 +0800

    Added demo e - operators

---
+++ src/e/operators/Main.java
@@ -0,0 +1,15 @@
+package e.operators;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Arithmetic operators
+
+		// TODO 02: Comparison operators
+		
+		// TODO 03: Logical operators
+		
+		// TODO 04: Assignment operators
+
+	}
+}
