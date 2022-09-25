206bdafb0e7d6b12bc0e61202680eb6831065225
teral! <alter.orbino@orangeandbronze.com>
2022-09-25 20:45:40 +0800

    Added demo a - basic output

---
+++ src/a/basic_output/Main.java
@@ -0,0 +1,12 @@
+package a.basic_output;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Output "hello world" to the console.
+		
+		// TODO 02: Output "hello world" to a dialog.
+
+	}
+
+}
