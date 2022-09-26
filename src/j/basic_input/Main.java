b55e40f5ebbe42e30bb9eb9636806f6e1b09ceed
teral! <alter.orbino@orangeandbronze.com>
2022-09-26 22:27:44 +0800

    Added demo j - basic inputs

---
+++ src/j/basic_input/Main.java
@@ -0,0 +1,14 @@
+package j.basic_input;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: Input through Scanner class
+
+		
+		// TODO 02: Input through dialogs
+
+
+	}
+
+}
