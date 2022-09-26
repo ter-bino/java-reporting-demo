7be6e9b06a3b5b72fc55e2ee33ed17fe51c992ff
teral! <alter.orbino@orangeandbronze.com>
2022-09-26 16:02:11 +0800

    Added demo f - Strings with performance demo

---
+++ src/e/strings/Main.java
@@ -0,0 +1,15 @@
+package e.strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: String and it's methods
+		
+		// TODO 02: String concatenation
+		
+		// TODO 03: String pools
+
+		// TODO 04: StringBuilder class
+	}
+
+}
