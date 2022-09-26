9a79ad9e2fc55c30373f661b043ecc9a6026737a
teral! <alter.orbino@orangeandbronze.com>
2022-09-26 15:50:04 +0800

    Added prepared demo for reference variables

---
+++ src/c/data_types/ReferenceDemo.java
@@ -0,0 +1,15 @@
+package c.data_types;
+
+import java.util.Arrays;
+
+public class ReferenceDemo {
+	
+	public static void main(String[] args) {
+		//TODO 04: Two reference variable with the same object
+		int[] array1 = new int[] {1, 2, 3, 4, 5};
+		System.out.println("Array 1: " + Arrays.toString(array1));
+		
+		int[] array2 = array1;
+		System.out.println("Array 2: " + Arrays.toString(array2));
+	}
+}
