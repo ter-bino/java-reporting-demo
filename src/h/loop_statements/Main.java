8407294905ba2bbd39eeb655588f0bf00de89f23
teral! <alter.orbino@orangeandbronze.com>
2022-09-26 18:06:07 +0800

    Added demo h - loop statements

---
+++ src/h/loop_statements/Main.java
@@ -0,0 +1,16 @@
+package h.loop_statements;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 01: While loop
+		
+		// TODO 02: Do-While loop
+		
+		// TODO 03: For loop
+		
+		// TODO 04: For-each loop
+		// int[] numbers = new int[]{1, 2, 3, 4, 5};
+
+	}
+}
