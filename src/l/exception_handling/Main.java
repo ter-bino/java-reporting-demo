f8a4f7b05ee3e296361764fdbb99cab9ca60c6d3
teral! <alter.orbino@orangeandbronze.com>
2022-09-28 17:44:43 +0800

    Activity for exception handling.

--- src/l/exception_handling/Main.java
+++ src/l/exception_handling/Main.java
@@ -1,10 +1,18 @@
 package l.exception_handling;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 01: 
-
+		
+		Scanner in = new Scanner(System.in);
+		
+		// TODO 01: InputMismatchException
+		
+		// TODO 02: NumberFormatException
+		
+		in.close();
 	}
 
 }
