bc1304b30aafc83024d4585591c54c2bf9de0ede
teral! <alter.orbino@orangeandbronze.com>
2022-09-28 22:57:57 +0800

    Created sample problem and activity in classes and objects

--- src/m/classes_and_objects/Main.java
+++ src/m/classes_and_objects/Main.java
@@ -3,7 +3,13 @@ package m.classes_and_objects;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO 01: Instantiating objects.
+		
+		// TODO 02: Accessing attributes.
+		
+		// TODO 03: Using constructors to initialize attributes.
+		
+		// TODO 04: Calling instance methods.
 
 	}
 
