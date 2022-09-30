dc31b0d289ddf105bbf61e2795dcbf55468d9f78
teral! <alter.orbino@orangeandbronze.com>
2022-10-01 01:33:46 +0800

    Sample problem and activity for polymorphism

--- src/p/polymorphism/Main.java
+++ src/p/polymorphism/Main.java
@@ -3,8 +3,11 @@ package p.polymorphism;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO 01: Polymorphism - same method, done differently
+		
+		// TODO 02: Method overriding
+		
+		// TODO 03: Method overloading
 	}
 
 }
